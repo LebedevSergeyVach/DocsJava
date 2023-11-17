9fd4b1cf92fe7f8330bcf5d9b3353a1dbedbdf66
LebedevSergeyV <ob430y@yandex.ru>
2023-11-18 01:23:32 +0700

    Update MainActivity & res

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -22,17 +23,38 @@ public class MainActivity extends AppCompatActivity {
         double b = Double.parseDouble(((EditText)findViewById(R.id.coefficient_b)).getText().toString());
         double c = Double.parseDouble(((EditText)findViewById(R.id.coefficient_c)).getText().toString());
 
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
-        double squareOne = (-b + Math.pow(discriminant, 0.5)) / 2 * a;
-        double squareTwo = (-b - Math.pow(discriminant, 0.5)) / 2 * a;
+        double discriminant = Math.pow(b, 2) - (4 * a * c);
+        double squareOne = (-b - Math.sqrt(discriminant)) / (2 * a);
+        double squareTwo = (-b + Math.sqrt(discriminant)) / (2 * a);
 
         TextView answerDiscriminant = findViewById(R.id.discriminant);
         TextView answerSquareOne = findViewById(R.id.squareOne);
         TextView answerSquareTwo = findViewById(R.id.squareTwo);
 
+        if (discriminant < 0) {
+            @SuppressLint({"StringFormatInvalid", "LocalSuppress"})
+            String discriminantText = String.format(getResources().getString(R.string.discriminantZero), discriminant);
+            answerDiscriminant.setText(discriminantText);
 
-        answerDiscriminant.setText("Дискриминант: " + discriminant);
-        answerSquareOne.setText("Первый корень: " + squareOne);
-        answerSquareTwo.setText("Второй корень: " + squareTwo);
+            String SquareOneText = String.format(getResources().getString(R.string.Zero), discriminant);
+            answerSquareOne.setText(SquareOneText);
+
+            String SquareTwoText = String.format(getResources().getString(R.string.Zero), discriminant);
+            answerSquareTwo.setText(SquareTwoText);
+
+        } else {
+            String discriminantText = getResources().getString(R.string.discriminantText, String.valueOf(discriminant));
+            answerDiscriminant.setText(discriminantText);
+
+            String SquareOneText = getResources().getString(R.string.squareOne, String.valueOf(discriminant));
+            answerSquareOne.setText(SquareOneText);
+
+            if (squareTwo != squareOne) {
+                String SquareTwoText = getResources().getString(R.string.squareTwo, String.valueOf(discriminant));
+                answerSquareTwo.setText(SquareTwoText);
+            } else {
+                answerSquareTwo.setText("Второго корня нет!");
+            }
+        }
     }
 }
